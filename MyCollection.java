34b1da5bd9596232b7b3423f14bc8c4b6b49d38e
8142774298 <72378411+8142774298@users.noreply.github.com>
2022-08-22 11:51:37 +0530

    Create MyCollection.java

---
+++ MyCollection.java
@@ -0,0 +1,13 @@
+// Executed on https://www.programiz.com/java-programming/online-compiler/
+import java.util.*;
+class MyCollection{
+	public static void main(String[] args) {
+		Set<Integer> h=new HashSet<Integer>();
+		h.add(5);
+		h.add(2);
+		h.add(3);
+		h.add(1);
+		h.add(4);
+		System.out.println(h);
+	}
+}
